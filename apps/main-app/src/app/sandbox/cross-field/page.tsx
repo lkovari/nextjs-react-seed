19c899576be2b86a0bb348ca9f2b640b1bb93f39
lkovari <laszlo.kovary@gmail.com>
2025-08-06 21:43:38 +0200

    details about cross field validation

--- apps/main-app/src/app/sandbox/cross-field/page.tsx
+++ apps/main-app/src/app/sandbox/cross-field/page.tsx
@@ -2,7 +2,7 @@ export default function CrossFieldPage() {
   return (
     <>
       <h2 className="text-2xl font-semibold mb-2 text-center">Form with cross field validation</h2>
-      <p className="text-center">coming soon...</p>
+      <p className="text-center">The cross field validation is when you have a password frield and a confirm password when you sign up into a system</p>
     </>
   );
 }
