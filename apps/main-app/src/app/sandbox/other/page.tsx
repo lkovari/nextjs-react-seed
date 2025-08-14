2c2ab8da02659b98965f2b0c2bb849088aa80a54
lkovari <laszlo.kovary@gmail.com>
2025-08-14 15:59:38 +0200

    fix grammar mistake

--- apps/main-app/src/app/sandbox/other/page.tsx
+++ apps/main-app/src/app/sandbox/other/page.tsx
@@ -2,7 +2,7 @@ export default function HomePage() {
   return (
     <>
       <h2 className="text-2xl font-semibold mb-2 text-center">TBD.</h2>
-      <p className="text-center">later I will decide what will I show here</p>
+      <p className="text-center">later, I’ll decide what to show here.</p>
     </>
   );
 }
